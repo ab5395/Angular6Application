1210f6a864a3a7016dce96d54835c066fc5e6613
bajay <bajay@C229>
2018-07-02 18:05:58 +0530

    Changes

--- OnlineGiftWebApp/OnlineGiftWebApp.Web/ClientApp/src/app/register/register.model.ts
+++
@@ -1,9 +0,0 @@
-export class ResgiterModel {
-    username: string;
-    password: string;
-    confirmPassword: string;
-    firstName: string;
-    lastName: string;
-    role: string;
-    location: string;
-  }
